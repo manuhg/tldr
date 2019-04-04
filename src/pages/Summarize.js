3a4fd8f6dd8662c7712336fe9281c11b9b21d8a9
harshalbhatia244@gmail.com <harshalbhatia244@gmail.com>
2019-04-04 22:39:41 +0530

    Changes to make UI work

--- src/pages/Summarize.js
+++ src/pages/Summarize.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 
 class Summarize extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: null,
+    };
+  }
+
   sendData = () => {
     var formData = new FormData();
     const { inputText } = this.props;
     formData.append('document', inputText);
     console.log(inputText);
-    fetch('https://cloud.tldr.cool/', {
+    axios('https://cloud.tldr.cool/summarize', {
       method: 'POST',
-      body: formData,
-    }).then(function(response) {
-      console.log(response);
-    });
+      data: formData,
+      config: { headers: { 'Content-Type': 'multipart/form-data' } },
+    })
+      .then(response => {
+        this.setState({ response: response.data.data });
+      })
+      .catch(err => {
+        console.log('errr', err);
+      });
   };
   componentDidMount() {
     this.sendData();
   }
   render() {
-    return (
+    return this.state.response ? (
+      this.state.response
+    ) : (
       <div className="App container">
         <div className="row">
           <div className="col-md-8 col-offset-md-2">
-            <div className="text-center" />
+            <div className="text-center">WATING</div>
           </div>
         </div>
       </div>
