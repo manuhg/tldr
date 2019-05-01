045afa3842088db7f5ac37f73823f427fe84dfc7
kalpitha <kalpithavbekal1997@gmail.com>
2019-05-01 18:06:25 +0530

    Redesign Home and summarize page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faGithub } from '@fortawesome/free-brands-svg-icons'; //faLinkedin
 // import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { Input, Icon, Button } from 'antd';
+import { Input, Icon, Button, Tabs, Upload, message, Layout, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 import logo from 'img/logo.png';
+import 'css/Home.css';
+
+const Dragger = Upload.Dragger;
 const { TextArea } = Input;
+const TabPane = Tabs.TabPane;
+const Content = Layout;
+const Search = Input.Search;
+
+// function beforeUpload(file) {
+//   const isJPG = file.type === 'image/jpeg';
+//   if (!isJPG) {
+//     message.error('You can only upload JPG file!');
+//   }
+// }
 
 class Home extends Component {
+  state = {
+    fileList: [],
+    uploading: false,
+  };
+
+  handleUpload = () => {
+    const { fileList } = this.state;
+    fileList.forEach(file => {
+      let reader = new FileReader();
+      reader.readAsText(file);
+
+      reader.onload = e => {
+        this.setState({
+          fileList: [],
+          uploading: false,
+        });
+        message.success('upload successfully.');
+        this.props.updateText(e.target.result);
+      };
+    });
+    this.setState({
+      uploading: true,
+    });
+  };
+
   clearInput = () => {
     this.inputTextBox.focus();
-    this.updateText('');
+    this.props.updateText('');
   };
 
   onChangeInputText = e => {
@@ -18,12 +57,60 @@ class Home extends Component {
   };
 
   render() {
+    // const props = {
+    //   name: 'file',
+    //   multiple: false,
+    //   // accept: '.pdf',
+    //   onChange(info) {
+    //     const status = info.file.status;
+    //     if (status !== 'uploading') {
+    //       console.log(info.file, info.fileList);
+    //     }
+    //     if (status === 'done') {
+    //       message.success(`${info.file.name} file uploaded successfully.`);
+    //       let file = info.file;
+    //       let reader = new FileReader();
+    //       console.log(file);
+    //       // let data = reader.readAsDataURL(file);
+    //       // console.log(data);
+    //     } else if (status === 'error') {
+    //       message.error(`${info.file.name} file upload failed.`);
+    //     }
+    //   },
+    //   customRequest: ({ file, onSuccess }) => {
+    //     setTimeout(() => {
+    //       onSuccess('ok');
+    //     }, 0);
+    //   },
+    // };
+
+    const { uploading, fileList } = this.state;
+    const props = {
+      accept: '.txt',
+      onRemove: file => {
+        this.setState(state => {
+          const index = state.fileList.indexOf(file);
+          const newFileList = state.fileList.slice();
+          newFileList.splice(index, 1);
+          return {
+            fileList: newFileList,
+          };
+        });
+      },
+      beforeUpload: file => {
+        this.setState(state => ({
+          fileList: [...state.fileList, file],
+        }));
+        return false;
+      },
+      fileList,
+    };
     const inputText = this.props.inputText;
     return (
       <div className="App container">
         <div className="row">
           <div className="col-12">
-            <img style={{ borderRadius: '50%', width: '150px' }} src={logo} alt="tl;dr" />
+            <img style={{ bordeRadius: '50%', width: '150px' }} src={logo} alt="tl;dr" />
             {/* <h2>Not all long things are nice</h2> */}
             {/* <div>
               {links.map((link, i) => (
@@ -45,40 +132,86 @@ class Home extends Component {
             <div className="text-center">
               <div style={{ fontSize: '1em', padding: '10px' }}>
                 <div>
-                  {/* <Search
-                    placeholder="input search text"
-                    enterButton="Summarize"
-                    size="large"
-                    onSearch={value => console.log(value)}
-                  /> */}
-                  {/* <Dragger {...props}>
-                    <p className="ant-upload-drag-icon">
-                      <Icon type="inbox" />
-                    </p>
-                    <p className="ant-upload-text">Click or drag and drop file to upload</p>
-                  </Dragger>
-                  <br /> */}
-                  <TextArea
-                    rows={8}
-                    placeholder=""
-                    value={inputText}
-                    onChange={this.onChangeInputText}
-                    ref={node => (this.inputTextBox = node)}
-                  />
-                </div>
-                <br />
-                <div>
-                  <Button onClick={this.clearInput}>Clear</Button>
-                  &nbsp;
-                  <Link
-                    to={{
-                      pathname: '/summarize',
-                    }}
-                  >
-                    <Button type="primary">Summarize</Button>
-                  </Link>
+                  <Tabs defaultActiveKey="1" type="card">
+                    <TabPane tab="Paste Text" key="1">
+                      <div>
+                        <TextArea
+                          rows={8}
+                          placeholder=""
+                          value={inputText}
+                          onChange={this.onChangeInputText}
+                          ref={node => (this.inputTextBox = node)}
+                        />
+                      </div>
+                      &nbsp;
+                      <div>
+                        <Button onClick={this.clearInput}>Clear</Button>
+                        &nbsp;
+                        <Link
+                          to={{
+                            pathname: '/summarize',
+                          }}
+                        >
+                          <Button type="primary" disabled={!this.props.inputText}>
+                            Summarize
+                          </Button>
+                        </Link>
+                      </div>
+                    </TabPane>
+                    <TabPane tab="Paste URL" key="2" disabled>
+                      <Row>
+                        <Col span={16}>
+                          <Input placeholder="Paste URL" />
+                        </Col>
+
+                        <Col span={8}>
+                          <Link
+                            to={{
+                              pathname: '/summarize',
+                            }}
+                          >
+                            <Button type="primary" disabled={!this.props.inputText}>
+                              Summarize
+                            </Button>
+                          </Link>
+                        </Col>
+                      </Row>
+                    </TabPane>
+                    <TabPane tab="Upload File" key="3">
+                      <div className="text-center mt-5">
+                        <Dragger {...props}>
+                          <p className="ant-upload-drag-icon">
+                            <Icon type="inbox" />
+                          </p>
+                          <p className="ant-upload-text">Click or drag file to upload</p>
+                        </Dragger>
+                        <Button
+                          type="primary"
+                          onClick={this.handleUpload}
+                          disabled={fileList.length === 0 || fileList.length > 1}
+                          loading={uploading}
+                          style={{ marginTop: 16 }}
+                        >
+                          {uploading ? 'Uploading' : 'Start Upload'}
+                        </Button>
+                      </div>
+                      &nbsp;
+                      <div>
+                        <Link
+                          to={{
+                            pathname: '/summarize',
+                          }}
+                        >
+                          <Button type="primary" disabled={!this.props.inputText}>
+                            Summarize
+                          </Button>
+                        </Link>
+                      </div>
+                    </TabPane>
+                  </Tabs>
                 </div>
               </div>
+
               <div>&nbsp;</div>
             </div>
           </div>
--- src/pages/Summarize.js
+++ src/pages/Summarize.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb, Icon, Row, Col, Collapse } from 'antd';
+import 'css/Summarize.css';
+import logo from 'img/logo.png';
+import { jQuery, $ } from 'jquery';
+
+const { Header, Content, Footer } = Layout;
+const Panel = Collapse.Panel;
 const axios = require('axios');
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
 
 class Summarize extends Component {
   constructor(props) {
@@ -31,16 +43,68 @@ class Summarize extends Component {
   }
   render() {
     return this.state.response ? (
-      this.state.response
+      <div>
+        <Layout>
+          <Header>
+            <img style={{ bordeRadius: '50%', width: '50px' }} src={logo} alt="tl;dr" />
+          </Header>
+          <Content style={{ padding: '30px', background: '#fff' }}>
+            <Collapse
+              bordered={false}
+              defaultActiveKey={['2']}
+              expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
+            >
+              <Panel
+                header="Original Text"
+                key="1"
+                style={{
+                  background: '#f7f7f7',
+                  borderRadius: '4',
+                  marginBottom: '24',
+                  border: '0',
+                  overflow: 'hidden',
+                }}
+              >
+                <div>{this.props.inputText}</div>
+              </Panel>
+              <br />
+              <Panel
+                header="Summarized Text"
+                key="2"
+                style={{
+                  background: '#f7f7f7',
+                  borderRadius: '4',
+                  marginBottom: '24',
+                  border: '0',
+                  overflow: 'hidden',
+                }}
+              >
+                <div>{text}</div>
+              </Panel>
+            </Collapse>
+          </Content>
+        </Layout>
+      </div>
     ) : (
       <div className="App container">
         <div className="row">
           <div className="col-md-8 col-offset-md-2">
-            <div className="text-center">WATING</div>
+            <div className="text-center">WAITING</div>
           </div>
         </div>
       </div>
     );
+    // return this.state.response ? (
+    //   this.state.response
+    // ) : (
+    //   <div className="App container">
+    //     <div className="row">
+    //       <div className="col-md-8 col-offset-md-2">
+    //         <div className="text-center">WAITING</div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // );
   }
 }
 export default Summarize;
